d20dcb816d26128dca42c8d2d894e1d36ba49b4e
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-22 10:53:06 +0700

    menambah case 1 yang berisi output dan input

--- Stack.cpp
+++ Stack.cpp
@@ -79,7 +79,10 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            
+           case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                
         }
         cout << endl;  
     } 
