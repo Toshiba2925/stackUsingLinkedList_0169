ed866fe4cb8db6aabb1ed88df064f3c999b89fb0
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-22 10:18:24 +0700

    menambah operasi peek/top pada class stack

--- Stack.cpp
+++ Stack.cpp
@@ -43,5 +43,20 @@ public:
         cout << "Popped value: " << top->data << endl;
     }
 
+    //peek/top operation: retrieve the value of the topmost element without removing
+    void peek(){
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node *current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }// return the value of the top node
+    }
+
     
 };
