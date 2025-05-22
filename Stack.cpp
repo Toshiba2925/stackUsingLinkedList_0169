8a184838fe2d492184419d234d46cf814c755c84
Toshiba2925 <aqifashiba25@gmail.com>
2025-05-22 11:00:19 +0700

    menambah case 3 dengan fungsi if

--- Stack.cpp
+++ Stack.cpp
@@ -92,7 +92,10 @@ int main() {
                     cout << "Stack is empty. Cannot pop." << endl;
                 }
                 break;
-            
+           case 3:
+                if (!stack.isEmpty()) {
+                    stack.peek(); //get the value of the top element
+                
         }
         cout << endl;  
     } 
